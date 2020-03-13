0da8a672b2a58ef3ca1ea192829e8844bfa12465
Kieran-Reilly <kieranpreilly@gmail.com>
2020-03-13 15:52:50 +0200

    Fix for Transiition pushing elements out
    A fix to keep the elements in the correct position once the aligns have been altered in the mobile view, and the window size is returned to greater than 769pxs.
    Looking to tidy up the solution so that it is more elegant

--- AsideTransition.js
+++ AsideTransition.js
@@ -1,10 +1,12 @@
 function leftAsideOpenTransition(){
     console.log("leftOpen");
+    document.getElementById("leftAside").style.transition = "0.5s";
     document.getElementById("leftAside").style.transform = "translateX(0%)";
 }
 
 function rightAsideOpenTransition(){
     console.log("rightOpen");
+    document.getElementById("rightAside").style.transition = "0.5s";
     document.getElementById("rightAside").style.transform = "translateX(0%)";
 }
 
@@ -16,4 +18,18 @@ function leftAsideCloseTransition(){
 function rightAsideCloseTransition(){
     console.log("RightClose");
     document.getElementById("rightAside").style.transform = "translateX(99%)";
+}
+
+function resizelistener() {
+    if(window.outerWidth > 768){
+        document.getElementById("leftAside").style.transition = "0s";
+        document.getElementById("rightAside").style.transition = "0s";
+        document.getElementById("leftAside").style.transform = "translateX(0%)";
+        document.getElementById("rightAside").style.transform = "translateX(0%)";
+    }else{
+        document.getElementById("leftAside").style.transition = "0s";
+        document.getElementById("rightAside").style.transition = "0s";
+        document.getElementById("leftAside").style.transform = "translateX(-99%)";
+        document.getElementById("rightAside").style.transform = "translateX(99%)";
+    }
 }
